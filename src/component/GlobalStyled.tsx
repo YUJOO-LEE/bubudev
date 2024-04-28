b67d0f7f830c7204d5405601037df17d31bd4cb0
YUJOO-LEE <lllllllllee@gmail.com>
2024-04-28 22:53:01 +0900

    feat: 첫화면 디자인

--- src/component/GlobalStyled.tsx
+++ src/component/GlobalStyled.tsx
@@ -2,10 +2,15 @@ import { css, Global } from '@emotion/react';
 
 const GlobalStyled = () => {
   return <Global styles={css`
+    @import url('https://fonts.googleapis.com/css2?family=Jua&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Jua&family=Noto+Sans+KR:wght@100..900&display=swap');
+    
     * {
       box-sizing: border-box;
       padding: 0;
       margin: 0;
+      font-family: "Noto Sans KR", sans-serif;
+      font-optical-sizing: auto;
     },
     ul,li,ol {
       list-style: none;
